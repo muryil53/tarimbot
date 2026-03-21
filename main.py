0e02e335cb034ddab954d4879a01af06b2488b02
muryil53 <muryil@gmail.com>
2026-03-22 00:50:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -137,5 +137,5 @@ def webhook_al():
 
 # ── ÇALIŞTIR ─────────────────────────────────────────
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
